d67902febee25c7393a3989f050935d3d7d905d1
Dimple <117870727+OneAnon@users.noreply.github.com>
2024-03-15 08:38:47 -0400

    current: time day month and year

--- scripts/clock_script.js
+++ scripts/clock_script.js
@@ -1,2 +1,15 @@
 var time = document.querySelector(".time");
 var dateTime = document.querySelector(".date-time");
+
+function updateClock()  {
+    var now = new Date();
+      var hours = now.getHours();
+      var minutes = now.getMinutes();
+      var seconds = now.getSeconds();
+      var day = now.getDay();
+      var date = now.getDate();
+      var month = now.getMonth();
+      var year = now.getFullYear();
+
+      
+}
