734211d44875fa6d63553cf11b9edf5e1fb6a5c8
Dimple <117870727+OneAnon@users.noreply.github.com>
2024-06-09 20:51:45 -0400

    commented out old clock js

--- scripts/clock_script.js
+++ scripts/clock_script.js
@@ -1,29 +1,33 @@
+/* 
 var time = document.querySelector(".time");
 var dateTime = document.querySelector(".date-time");
+*/
 
-function updateClock()  {
+//function updateClock()  {
     /* Gets the current time and date */
-      var now = new Date();
-      var hours = now.getHours();
-      var minutes = now.getMinutes();
-      var seconds = now.getSeconds();
-      var day = now.getDay();
-      var date = now.getDate();
-      var month = now.getMonth();
-      var year = now.getFullYear();
-    /* stores the day and month in an array*/
-      var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-      var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//      var now = new Date();
+//      var hours = now.getHours();
+//      var minutes = now.getMinutes();
+//      var seconds = now.getSeconds();
+//      var day = now.getDay();
+//      var date = now.getDate();
+//      var month = now.getMonth();
+//      var year = now.getFullYear();
+   /* stores the day and month in an array*/
+//      var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//      var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     /* formats the date and time*/
-      hours = hours % 12 || 12;
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-      date = date < 10 ? "0" + date : date;
+//      hours = hours % 12 || 12;
+//      minutes = minutes < 10 ? "0" + minutes : minutes;
+//      seconds = seconds < 10 ? "0" + seconds : seconds;
+//      date = date < 10 ? "0" + date : date;
     /* displays the date and time */
-      var period = hours > 12 ? "AM" : "PM"; // reverted change
-      time.innerHTML = hours + ":" + minutes + ":" + seconds + " " + period;
-      dateTime.innerHTML = dayNames[day] + ", " + monthNames[month] + " " + date + ", " + year; 
-}
-
+//      var period = hours > 12 ? "AM" : "PM"; // reverted change
+//      time.innerHTML = hours + ":" + minutes + ":" + seconds + " " + period;
+//      dateTime.innerHTML = dayNames[day] + ", " + monthNames[month] + " " + date + ", " + year; 
+//}
+/*
 updateClock();
     setInterval(updateClock, 1000); 
+*/
+
