14645ee5fbe02e802ff1e9008553614ea0fd0802
Dimple <117870727+OneAnon@users.noreply.github.com>
2024-06-10 00:27:40 -0400

    added else if to hours for 00

--- scripts/clock_script.js
+++ scripts/clock_script.js
@@ -35,6 +35,11 @@ function updateClock() {
   var now = new Date();
   var date = now.toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" });
   var hours = now.getHours().toString();
+  if (hours ==="00") {
+    hours = "00";
+  } else if (hours > 12) {
+    hours = (hours % 12).toString();
+  }
   var minutes = now.getMinutes().toString();
   var seconds = now.getSeconds().toString();
   document.getElementById("hours").textContent = hours;
