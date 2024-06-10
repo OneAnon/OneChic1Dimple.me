522505ecc6f30054470fe3c1ca0923b5c1afc9d2
Dimple <117870727+OneAnon@users.noreply.github.com>
2024-06-09 21:35:08 -0400

    added seconds to js

--- scripts/clock_script.js
+++ scripts/clock_script.js
@@ -36,6 +36,7 @@ function updateClock() {
   var date = now.toLocaleDateString();
   var hours = now.getHours().toString().padStart(2,"0");
   var minutes = now.getMinutes().toString().padStart(2,"0");
+  var seconds = now.getSeconds().toString().padStart(2,"0");
   document.getElementById("hours").textContent = hours;
   document.getElementById("minutes").textContent = minutes;
   document.getElementById("seconds").textContent = seconds;
